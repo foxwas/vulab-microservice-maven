8167ff0d204347aa82ed40091252d6109ddd40e8
saravanan <saravanan_kuppusamy@yahoo.com>
2024-03-29 16:43:30 -0400

    updated the repo

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('sofia', 'pass', 'sofia@visa.com');
-insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('saravanan', 'pass', 'sarkuppu@visa.com');
-insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('eduardo', 'pass', 'eduardo@visa.com');
-insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('shah', 'pass', 'shah@visa.com');
-
-
